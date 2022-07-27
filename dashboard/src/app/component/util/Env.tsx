14fbe52e67f4bc32f2ff93d99aed32c2024a1768
James Phillips <jamesdphillips@gmail.com>
2022-07-27 11:03:19 -0700

    more hacking
    Signed-off-by: James Phillips <jamesdphillips@gmail.com>

--- dashboard/src/app/component/util/Env.tsx
+++ dashboard/src/app/component/util/Env.tsx
@@ -5,9 +5,10 @@ export namespace Env {
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
   export interface Values {}
 
-  export type ProviderProps<T extends Values> = Omit<Partial<T>, "children"> & {
+  export interface ProviderProps<T extends Values> {
     children: React.ReactNode;
-  };
+    value: T;
+  }
 
   export interface ConsumerProps<T extends Values> {
     children: (_: T) => React.ReactElement<any, any>;
@@ -17,9 +18,9 @@ export namespace Env {
 
   export function Provider<T extends Values>({
     children,
-    ...props
+    value,
   }: ProviderProps<T>) {
-    return <Context.Provider value={props}>{children}</Context.Provider>;
+    return <Context.Provider value={value}>{children}</Context.Provider>;
   }
 
   export function Consumer<T extends Values>(props: ConsumerProps<T>) {
