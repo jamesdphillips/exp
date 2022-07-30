396b262f6bf4531707bbe9e370f6cd34bf9f6540
James Phillips <jamesdphillips@gmail.com>
2022-07-29 21:24:22 -0700

    Revert "Revert "observable""
    This reverts commit 288746c8eaaadc44dc77deae07e7d7eb8eb02a3a.

--- dashboard/src/app/primitive/Atom.ts
+++ dashboard/src/app/primitive/Atom.ts
@@ -22,10 +22,11 @@ type Unsubscribe = () => void;
 
 interface Observable {
   /**
-   * Subscribe to updates for a specific key
+   * Subscribe to updates for a specific key.
    *
    * @param key
-   * @param callback is called when update occurs
+   * @param callback receives call when event occurs
+   * @returns function used to unsubscribe from events
    */
   subscribe(callback: (_: this) => void): Unsubscribe;
 }
@@ -33,6 +34,8 @@ interface Observable {
 export type Atom<T> = Gettable<T> & Settable<T>;
 
 export type ObservableAtom<T> = Atom<T> & Observable;
+
+export type MaybeObservableAtom<T> = Atom<T> & Partial<Observable>;
 
 class atom<T> {
   #value: T;
@@ -50,7 +53,56 @@ class atom<T> {
   }
 }
 
-export function create<T>(): Atom<T | undefined>;
+export function create<T>(initial: undefined): Atom<T | undefined>;
 export function create<T>(initial: T): Atom<T> {
   return new atom(initial);
+}
+
+class observer<T> {
+  #subscriptions: Set<(_: T) => void> = new Set();
+
+  subscribe(callback: (_: T) => void): Unsubscribe {
+    this.#subscriptions.add(callback);
+    return () => this.#subscriptions.delete(callback);
+  }
+
+  notify(value: T) {
+    this.#subscriptions.forEach((subscription) => subscription(value));
+  }
+}
+
+class observableAtom<T> {
+  #delegate: Atom<T>;
+  #observer: observer<this>;
+
+  constructor(delegate: Atom<T>) {
+    this.#delegate = delegate;
+    this.#observer = new observer();
+  }
+
+  get(): T {
+    return this.#delegate.get();
+  }
+
+  set(value: T) {
+    this.#delegate.set(value);
+    this.#observer.notify(this);
+  }
+
+  subscribe(callback: (_: this) => void): Unsubscribe {
+    return this.#observer.subscribe(callback);
+  }
+}
+
+/**
+ * Updates given atom allowing consumers to subscribe to updates.
+ *
+ * @param atom
+ * @returns observable atom
+ */
+export function watch<T>(atom: Atom<T>): ObservableAtom<T> {
+  if ("subscribe" in atom) {
+    return atom;
+  }
+  return new observableAtom(atom);
 }
